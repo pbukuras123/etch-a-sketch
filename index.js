7e344d60982e1e740f8a10cf58e91ddfa17d18b2
Peter Bukuras <peterbukuras@gmail.com>
2022-09-12 16:59:50 -0400

    finish 16x16 grid with gray, black, and color

---
+++ index.js
@@ -0,0 +1,69 @@
+const container = document.querySelector('.container');
+const btnBlack = document.createElement('button')
+const btnGray = document.createElement('button')
+const btnRGB = document.createElement('button')
+const btnSize = document.createElement('button')
+const buttonsContainer = document.querySelector('.buttons')
+
+function createDivs(col, rows) {
+    for(let i = 0 ; i < (col * rows); i++) {
+        const div = document.createElement('div')
+        div.style.border = '1px solid red'
+        container.style.gridTemplateColumns = `repeat(${col}, 1fr)`;
+        container.style.gridTemplateRows = `repeat(${rows}, 1fr)`;
+        container.appendChild(div).classList.add('box')
+        
+    }
+}
+
+createDivs(16,16)
+
+
+
+function grayColor() {
+    const boxs = container.querySelectorAll(`.box`)
+    btnGray.textContent = "Gray"
+    btnGray.addEventListener('click', () => {
+        boxs.forEach(box => box.addEventListener('mouseover', () => {
+            let Rnum = Math.floor(Math.random() * 255)
+            box.style.background = `rgb(${Rnum}, ${Rnum}, ${Rnum})`
+        }))
+    })
+
+    buttonsContainer.appendChild(btnGray).classList.add('btn')
+}
+
+grayColor()
+
+function blackColor() {
+    const boxs = container.querySelectorAll(`.box`)
+    btnBlack.textContent = "Black"
+    btnBlack.addEventListener('click', () => {
+        boxs.forEach(box => box.addEventListener('mouseover', () => {
+            box.style.background = `black`
+        }))
+    })
+
+    buttonsContainer.appendChild(btnBlack).classList.add('btn')
+}
+
+blackColor()
+
+function rgbColor() {
+    const boxs = container.querySelectorAll(`.box`)
+    btnRGB.textContent = "RGB"
+    btnRGB.addEventListener('click', () => {
+        boxs.forEach(box => box.addEventListener('mouseover', () => {
+            let R = Math.floor(Math.random() * 256)
+            let B = Math.floor(Math.random() * 256)
+            let G = Math.floor(Math.random() * 256)
+            box.style.background = `rgb(${R}, ${G}, ${B})`
+
+        }))
+
+    })
+
+    buttonsContainer.appendChild(btnRGB).classList.add('btn')
+}
+
+rgbColor()
